bc1745569dc71e5e713e23b74190f1890a9a5c37
Carlos Matos <118901076+cmatosbc@users.noreply.github.com>
2025-02-18 01:14:46 +0000

    Update command.ts

--- bin/command.ts
+++ bin/command.ts
@@ -1,16 +1,31 @@
-#!/usr/bin/env node
-import { startLogging, stopLogging } from './commandLogger';
+import { createLogger, LogMessage } from './logger';
 
-const args = process.argv.slice(2);
+const logger = createLogger({
+  outputStream: process.stdout,
+  logLevel: 'info',
+});
 
-const command = args[0];
+let logging = false;
 
-if (command === 'init') {
-  startLogging();
-  console.log('TSL logging started...');
-} else if (command === 'halt') {
-  stopLogging();
-  console.log('TSL logging stopped.');
-} else {
-  console.log(`Unknown command: ${command}`);
-}
+export const startLogging = () => {
+  logging = true;
+  process.on('beforeExit', logCommand);
+};
+
+export const stopLogging = () => {
+  logging = false;
+  process.removeListener('beforeExit', logCommand);
+};
+
+const logCommand = (code: number) => {
+  if (!logging) return;
+
+  const command = process.argv.slice(2).join(' ');
+  if (/node|\.js/.test(command)) {
+    const logMessage: LogMessage = {
+      level: 'info',
+      message: `Command executed: ${command}`,
+    };
+    logger.log(logMessage);
+  }
+};
